689a3885dc97b5aedc7d8075457ba21dcb797df6
AlejandrodePablo <depablocorreal@gmail.com>
2024-10-09 16:49:59 +0200

    Olvidado subir SuperHeroDetailActivity

--- app/src/main/java/edu/iesam/dam2024/features/superhero/presentation/SuperHeroDetailActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/features/superhero/presentation/SuperHeroDetailActivity.kt
@@ -1,20 +1,8 @@
 package edu.iesam.dam2024.features.superhero.presentation
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
-import coil.load
-import com.bumptech.glide.Glide
 import edu.iesam.dam2024.R
-import edu.iesam.dam2024.app.extensions.loadUrl
-import edu.iesam.dam2024.features.superhero.domain.SuperHero
 
 class SuperHeroDetailActivity : AppCompatActivity() {
 
@@ -23,7 +11,7 @@ class SuperHeroDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_super_hero_detail)
+        setContentView(R.layout.activity_superhero_detail)
 
     }
 
